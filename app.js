7e8e5654ca2385b5e23ede9550df1a1d21231c61
Daniel Antunes de Almeida <62892836+dan-dealmeida@users.noreply.github.com>
2023-10-23 13:21:01 -0300

    Update .gitignore to exclude certain files

--- app.js
+++ app.js
@@ -1,61 +1,23 @@
-const request = require('request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const urlWeather = 'http://api.weatherstack.com/current?access_key=ba23243d98c8e9f4958bcac1b4d00f4c&query=37.8267,-122.4233'
-const urlMap = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGFuaWVsZGVhbG1laWRhIiwiYSI6ImNsbXM1M2lhZjAwc24ycnA5ZXhxdnpuZjQifQ.MFMxCnC6mfFfQy6zhC8MJg&limit=1'
-
-
-
-// request({url:urlWeather, json:true}, (error, response) =>{
-//     if (error) {
-//         console.log('Unable to connect to weather service')
-//     } else if(response.body.error){
-//         console.log('Unable to find location')
-//     }else{
-//         const temp = response.body.current.temperature
-//         const precip = response.body.current.precip
-//         const weather_description = response.body.current.weather_descriptions[0]
-//
-//         console.log(weather_description + '. It is currently '+ temp+ ' degrees out. There is a ' + precip + '% chance of rain.' )
-//     }
-//
-// })
-
-//Geocoding
-// request({url:urlMap, json:true}, (error, response) =>{
-//     if (error){
-//         console.log('Unable to connect to location services')
-//     } else if (response.body.features.length === 0){
-//         console.log('No matches found')
-//     } else{
-//         const lat = response.body.features[0].center[1]
-//         const long = response.body.features[0].center[0]
-//
-//         console.log('latitude: '+lat)
-//         console.log('longitude: '+long)
-//     }
-// })
-
-
-const geoCode = (adress, callback) => {
-    const url  = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(adress) +  '.json?access_token=pk.eyJ1IjoiZGFuaWVsZGVhbG1laWRhIiwiYSI6ImNsbXM1M2lhZjAwc24ycnA5ZXhxdnpuZjQifQ.MFMxCnC6mfFfQy6zhC8MJg&limit=1'
-    request ({url:url, json:true}, (error, response) =>{
-        if(error){
-            callback('Unable to connect to location services.', undefined)
-        } else if(response.body.features.length === 0){
-            callback('Unable to find location. Try another search', undefined)
-        } else{
-            callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
-                location: response.body.features[0].place_name
-            })
-
+const adress = process.argv[2]
+if (!adress) {
+    console.log('Please provide an adress')
+} else {
+    geocode(adress, (error, data) => {
+        if (error){
+            return console.log(error)
         }
+
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error){
+                return console.log(error)
+            }
+
+            console.log(data.location)
+            console.log(forecastData)
+        })
     })
 }
-
-geoCode('New York', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
 
